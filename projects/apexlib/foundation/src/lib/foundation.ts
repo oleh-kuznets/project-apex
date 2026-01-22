e1b4bf3f6c2160c1ea435a1eb5a79767c5362b37
Oleh Kuznets <oleh.kuznets@gmail.com>
2026-01-22 20:56:46 +0200

    lesson-2.2/setup-storybook-project

---
+++ projects/apexlib/foundation/src/lib/foundation.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-foundation',
+  imports: [],
+  template: `
+    <p>
+      foundation works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Foundation {
+
+}
